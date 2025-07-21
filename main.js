b39697ceb980e8f750b5e39bfb78547ef76035ec
ayush-vishwakarma714 <ayushvishwakarma1728@gmail.com>
2025-07-21 16:25:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
   searchForm.classList.toggle('active');
+  shoppingCart.classList.remove('active');
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
+
 };
 
 
@@ -9,6 +13,9 @@ let shoppingCart = document.querySelector('.shopping-cart-a');
 
 document.querySelector('#cart-btn').onclick = () => {
   shoppingCart.classList.toggle('active');
+  searchForm.classList.remove('active');
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
 };
 
 
@@ -16,4 +23,26 @@ let loginForm = document.querySelector('.header .login-form');
 
 document.querySelector('#login-btn').onclick = () => {
   loginForm.classList.toggle('active');
+  searchForm.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  navbar.classList.remove('active');
 };
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+ navbar.classList.toggle('active');
+ searchForm.classList.remove('active');
+ shoppingCart.classList.remove('active');
+ loginForm.classList.remove('active');
+};
+
+
+window.onscroll = () => 
+  {
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+ };
+ 
